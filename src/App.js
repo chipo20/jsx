63f855a8e5225aaca7b574c77b6ef7d55305bd8d
khawla chouchene bouhmila <70218120+khawlaChoucheneBouhmila@users.noreply.github.com>
2021-04-13 18:08:32 +0200

    jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+import './style.css';
+
+import imageInSrc from './ImageInSrc.jpg';
+import Myvideo from './Myvideo.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   <div>
+
+     <h1 className="title red">Dribek Chiheb</h1>
+
+<br/>
+
+<img classeName="img1" src={imageInSrc}  alt="image_Insrc" />
+
+<br/>
+
+<img  classeName="img2"src="./ImageInPublic.jpg" alt="image_public" />
+</div> 
+<video classeName="vd" width="320" height="240" controls>
+
+ <source  src={Myvideo} type="video/mp4" />
+    </video>
+
+     
     </div>
   );
 }
